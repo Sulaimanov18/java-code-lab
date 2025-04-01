34a87ce2b8af0794a0f6a3b3811869b0973244ad
Timur Sulaimanov <tim.sulaimanov@stablekernel.com>
2025-03-31 21:52:50 -0500

    fibonacci method

---
+++ src/main/java/day_3/fibonacci.java
@@ -0,0 +1,32 @@
+package day_3;
+
+public class fibonacci {
+    public static void main(String[] args) {
+
+        System.out.println(isFibonacci(2));
+        System.out.println(isFibonacci(1));
+        System.out.println(isFibonacci(13));
+        System.out.println(isFibonacci(5));
+        System.out.println(isFibonacci(6));
+        System.out.println(isFibonacci(11));
+        System.out.println(isFibonacci(10));
+
+
+
+    }
+
+    private static boolean isFibonacci(int num) {
+        int a = 0;
+        int b = 1;
+        while (a <= num) {
+            if (a == num) {
+                return true;
+            }
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return false;
+    }
+
+}
