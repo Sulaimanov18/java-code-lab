2918c7271fd76a94cac562f021b2d212601e9221
Timur Sulaimanov <Sulaimanov18@google.com>
2025-04-10 18:40:03 -0500

    New updates

--- src/main/java/day_4/count_words.java
+++ src/main/java/day_4/count_words.java
@@ -7,13 +7,20 @@ public class count_words {
 
     public static void main(String[] args) {
 
-        System.out.println(countLetters("aaaaaaa"));
+        System.out.println(countLetters("Timur"));
+        System.out.println(countLetters(""));
+        System.out.println(countLetters("11111"));
+        System.out.println(countLetters("aa"));
+        System.out.println(countLetters("a12a"));
+        System.out.println(countLetters("*&*@"));
+        System.out.println(countLetters("@#$@"));
 
     }
 
 
     private static Map<Character, Integer> countLetters(String str){
         Map<Character, Integer> count = new HashMap<>();
+        str = str.toLowerCase();
 
         for (Character ch : str.toCharArray()){
             if(Character.isLetter(ch)){
