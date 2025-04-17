1ed44d3a7924c4ec7334cbd890d91b96aeb914e7
Timur Sulaimanov <Sulaimanov1718@gmail.com>
2025-04-17 15:32:49 -0500

    new banking system

--- src/main/java/day_8/test_checkingAccount.java
+++ src/main/java/day_8/test_checkingAccount.java
@@ -6,8 +6,12 @@ public class test_checkingAccount {
         checkingAccount account = new checkingAccount(0);
         account.add(11);
         account.add(11);
-
         System.out.println(account.getBalance());
+        account.withdraw(4);
+        System.out.println(account.getBalance());
+        account.reset();
+        System.out.println(account.getBalance());
+
 
     }
 }
