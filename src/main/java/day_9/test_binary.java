09c58ad00791accfe345cabb5b05097bd2078c3f
Timur Sulaimanov <Sulaimanov1718@gmail.com>
2025-04-19 10:40:19 -0500

    implementation details

--- src/main/java/day_9/test_binary.java
+++ src/main/java/day_9/test_binary.java
@@ -1,4 +1,13 @@
 package day_9;
 
 public class test_binary {
+
+    public static void main(String[] args) {
+        body_binary body = new body_binary();
+        int [] arr = {1,4, 5,7,8, 10, 14, 20,40, 99 };
+        System.out.println(body.binarySearch(arr, 7));
+        System.out.println(body.binarySearch(arr, 20));
+        System.out.println(body.binarySearch(arr, 99));
+        System.out.println(body.binarySearch(arr, 1));
+    }
 }
